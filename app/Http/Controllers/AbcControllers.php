ee6b5c4ed1d56753b440f457a7ca17e27b6d7b66
dam hieu <damquanghieu1998@gmail.com>
2022-02-17 15:23:57 +0700

    test1

--- app/Http/Controllers/AbcControllers.php
+++ app/Http/Controllers/AbcControllers.php
@@ -11,6 +11,11 @@ class AbcControllers extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+     /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         //Display a listing of the resource.e resource.
